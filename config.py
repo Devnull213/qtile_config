cddacb9c059039c70b01be2ca381f71e8f8f9000
Guerra-Defranchi <guerra.defranchi@protonmail.com>
2022-01-31 21:59:01 -0300

    adding new configs to config.py

--- config.py
+++ config.py
@@ -48,7 +48,7 @@ mouse = [
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
-follow_mouse_focus = True
+follow_mouse_focus = False
 bring_front_click = False
 cursor_warp = False
 auto_fullscreen = True
